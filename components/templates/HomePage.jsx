2bd447549b432a79ceb3405cc4c304538cc4a46a
Ali Alipoor <shirdalldavani@gmail.com>
2024-03-02 15:23:35 +0330

    home page is finished

--- components/templates/HomePage.jsx
+++ components/templates/HomePage.jsx
@@ -1,6 +1,10 @@
 import Attributes from "../modules/Attributes";
 import Banner from "../modules/Banner";
+import Companies from "../modules/Companies";
 import Definition from "../modules/Definition";
+import Guide from "../modules/Guide";
+import Instruction from "../modules/Instruction";
+import Restrictions from "../modules/Restrictions";
 
 import styles from "./HomePage.module.css";
 
@@ -10,6 +14,10 @@ function HomePage() {
       <Banner />
       <Attributes />
       <Definition />
+      <Companies />
+      <Instruction />
+      <Guide />
+      <Restrictions />
     </div>
   );
 }
